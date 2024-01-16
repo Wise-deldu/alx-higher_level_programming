40a6deec81198e6e38ded21ff7b998b0b3d3c6dc
Wise-deldu <delduwise@gmail.com>
2024-01-16 20:20:06 +0000

    Remove 2 prints statements and added string format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
-for alpha in range(ord('Z'), ord('A') - 1, -1):
-    if alpha % 2 == 0:
-        print(chr(alpha + 32), end="")
-    else:
-        print(chr(alpha), end="")
+for alp in range(ord('Z'), ord('A') - 1, -1):
+    print("{}".format(chr(alp + 32) if alp % 2 == 0 else chr(alp)), end="")
