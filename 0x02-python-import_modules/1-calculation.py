f1e227d83decf8ae3cec53a2d2f4b757510f7059
Wise-deldu <delduwise@gmail.com>
2024-01-18 11:21:31 +0000

    Added main function

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
+from calculator_1 import add, sub, mul, div
 
-    from calculator_1 import add, sub, mul, div
-
+def main():
     a = 10
     b = 5
 
@@ -11,3 +10,6 @@ if __name__ == "__main__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
+
+if __name__ == "__main__":
+    main()
