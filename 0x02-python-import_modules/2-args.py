566ea596b627eda71b65ed1e6ff1d7e5be744136
Wise-deldu <delduwise@gmail.com>
2024-01-19 01:33:38 +0000

    Re-modify module and added colon to argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
+    from sys import argv
 
-    number = len(sys.argv) - 1
+    number = len(argv) - 1
     if number == 0:
         print("0 arguments.")
     elif number == 1:
-        print("1 argument.")
+        print("1 argument:")
     else:
         print("{} arguments:".format(number))
     for i in range(number):
-        print("{}: {}".format((i + 1), sys.argv[i + 1]))
+        print("{}: {}".format((i + 1), argv[i + 1]))
