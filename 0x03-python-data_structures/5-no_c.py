814429c2130c33575f4c6762007b4a7ab5fa90b9
Wise-deldu <delduwise@gmail.com>
2024-01-22 18:07:40 +0000

    Modified the code to use list comprehension

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    final_string = ""
-    remove_char = ['c', 'C']
-    
-    for i in my_string:
-        if i not in remove_char:
-            final_string += i
-    return final_string
+    final_string = [i for i in my_string if i != 'c' and i != 'C']
+    return ("".join(final_string))
