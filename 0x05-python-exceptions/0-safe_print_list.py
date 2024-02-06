3e1579d2cb429e2f7d467d35a4aee44c9a5b6ca5
Wise-deldu <delduwise@gmail.com>
2024-02-06 01:00:17 +0000

    A function that prints x elements of list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+def safe_print_list(my_list=[], x=0):
+    if my_list is None:
+        return 0
+    count = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            count = count + 1
+
+        print()
+    except IndexError:
+        print()
+        pass
+    return count
