aba454aa3846944faef07b3432683a8de4bf5666
Wise-deldu <delduwise@gmail.com>
2024-10-28 04:11:55 +0000

    JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+// First argument is the API URL
+const URL = process.argv[2];
+
+request(URL, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (body) {
+    // Wedge Antilles is character ID 18 - use this ID for filtering
+    // result of the API
+    const json = JSON.parse(body);
+    const charFilms = json.results.filter(
+      x => x.characters.find(y => y.match(/\/people\/18\/?$/))
+    );
+    console.log(charFilms.length);
+  }
+})
